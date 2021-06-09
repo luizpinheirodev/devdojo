d6988b19c6041344f3c3a4e679388317b5c59b2a
Luiz Geraldo Pinheiro <luiz_geraldo@sicredi.com.br>
2021-06-09 19:51:09 -0300

    Arrays

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05Arrays {
+public class Aula08ArraysMultidimensionais {
     public static void main(String[] args) {
         /**
          * Inicialização:
